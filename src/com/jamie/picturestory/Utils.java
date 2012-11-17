89cb946af720f262cc39f2f0462229ff7e638baf
Jamie Hewland <jhewland@gmail.com>
2012-11-17 11:29:11 +0200

    Utils cleanup

--- src/com/jamie/picturestory/Utils.java
+++ src/com/jamie/picturestory/Utils.java
@@ -40,17 +40,6 @@ public class Utils {
     private Utils() {};
 
     /**
-     * Workaround for bug pre-Froyo, see here for more info:
-     * http://android-developers.blogspot.com/2011/09/androids-http-clients.html
-     */
-    /*public static void disableConnectionReuseIfNecessary() {
-        // HTTP connection reuse which was buggy pre-froyo
-        if (hasHttpConnectionBug()) {
-            System.setProperty("http.keepAlive", "false");
-        }
-    }*/
-
-    /**
      * Get the size in bytes of a bitmap.
      * @param bitmap
      * @return size in bytes
@@ -77,23 +66,6 @@ public class Utils {
         }
         return true;
     }
-
-    /**
-     * Get the external app cache directory.
-     *
-     * @param context The context to use
-     * @return The external cache dir
-     */
-    /*@SuppressLint("NewApi")
-    public static File getExternalCacheDir(Context context) {
-        if (hasExternalCacheDir()) {
-            return context.getExternalCacheDir();
-        }
-
-        // Before Froyo we need to construct the external cache dir ourselves
-        final String cacheDir = "/Android/data/" + context.getPackageName() + "/cache/";
-        return new File(Environment.getExternalStorageDirectory().getPath() + cacheDir);
-    }*/
 
     /**
      * Check how much usable space is available at a given path.
@@ -173,32 +145,4 @@ public class Utils {
     	
     	return mediaStorageDir;
     }
-    
-    /**
-     * Check if OS version has a http URLConnection bug. See here for more information:
-     * http://android-developers.blogspot.com/2011/09/androids-http-clients.html
-     *
-     * @return
-     */
-    //public static boolean hasHttpConnectionBug() {
-    //    return Build.VERSION.SDK_INT < Build.VERSION_CODES.FROYO;
-    //}
-
-    /**
-     * Check if OS version has built-in external cache dir method.
-     *
-     * @return
-     */
-    //public static boolean hasExternalCacheDir() {
-    //    return Build.VERSION.SDK_INT >= Build.VERSION_CODES.FROYO;
-    //}
-
-    /**
-     * Check if ActionBar is available.
-     *
-     * @return
-     */
-    //public static boolean hasActionBar() {
-    //    return Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB;
-    //}
 }
